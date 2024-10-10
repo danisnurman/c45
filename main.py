6aaac6a3dea2e96ff4b945ad47cb23e5f566d5b0
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2024-10-10 20:59:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ streamlit.write(feature_cols)
 model = C45Classifier()
 model.fit(X, y)
 
-# # Predict
+# Predict
 # data_test = pd.read_csv('https://raw.githubusercontent.com/danisnurman/psbnd2/main/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
-# model.predict(data_test)
+model.predict(data)
 
-# # Evaluate
+# Evaluate
 # data_test = pd.read_csv('https://raw.githubusercontent.com/danisnurman/psbnd2/main/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
 # X_test = data_test.drop(['Diabetes_binary'], axis=1)
 # y_test = data_test['Diabetes_binary']
-# streamlit.write(model.evaluate(X_test, y_test))
+model.evaluate(X, y)
 
 # # Summary Model
-# streamlit.write(model.summary())
+model.summary()
+streamlit.write(model.summary())
 
 streamlit.write("Testing")
